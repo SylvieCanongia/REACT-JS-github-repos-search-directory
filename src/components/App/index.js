cf02deddb7eb492ae7d96c55cc4d731fa66262ff
Sylvie Canongia <sylvie.canongia.dev@gmail.com>
2021-05-27 11:04:02 +0200

    base styles and app title

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -3,11 +3,15 @@ import React from 'react';
 
 // == Import
 import './styles.scss';
+import logo from 'src/assets/images/logo-github.png';
 
 // == Component
 const App = () => (
   <div className="app">
-    <h1>Composant : App</h1>
+    <h1 className="app__title"><img src={logo} alt="Github logo" /></h1>
+    <div>SearchBar</div>
+    <div>Message</div>
+    <div>ReposResults</div>
   </div>
 );
 
