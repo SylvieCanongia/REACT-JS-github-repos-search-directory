75934775693f9344ce21c6149f8365c7391d7eec
Sylvie Canongia <sylvie.canongia.dev@gmail.com>
2021-05-28 18:02:55 +0200

    addition of a semantic ui Message component

--- src/components/Message/index.js
+++ src/components/Message/index.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import { Message as MessageUi } from 'semantic-ui-react';
 
 import './message.scss';
 
 const Message = () => (
-  <div className="message">Voici les 30 premiers résultats sur 2134843 résultats.</div>
+  <MessageUi id="message">
+    Voici les 30 premiers résultats sur 2134843 résultats.
+  </MessageUi>
+  // <div ></div>
 );
 
 export default Message;
